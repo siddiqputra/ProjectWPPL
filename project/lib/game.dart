93523103aa7a7ebf76427eacf2a26bdcf9d9438e
Siddiq <127266291+siddiqputra@users.noreply.github.com>
2024-12-12 17:45:43 +0700

    Update game.dart

--- project/lib/game.dart
+++ project/lib/game.dart
@@ -1,22 +1,13 @@
 // Mengimpor pustaka Dart untuk menangani operasi asynchronous dan timer.
 import 'dart:async';
-<<<<<<< HEAD
-
 // Mengimpor paket Flame untuk pengembangan game.
-=======
 import 'package:flame/events.dart';
->>>>>>> 2d5595c8838bfa7332e170da5e6f357631aac882
 import 'package:flame/game.dart';
 // Mengimpor kelas Bird dari file komponen dalam proyek.
 import 'package:project/components/bird.dart';
 
-<<<<<<< HEAD
-// Mendefinisikan kelas FlappyBirdGame yang merupakan subclass dari FlameGame.
-// FlameGame menyediakan kerangka kerja dasar untuk pengembangan game.
-class FlappyBirdGame extends FlameGame {
-=======
-class FlappyBirdGame extends FlameGame with TapDetector{
->>>>>>> 2d5595c8838bfa7332e170da5e6f357631aac882
+class FlappyBirdGame extends FlameGame with TapDetector {
+}
   /*
    Komponen dasar game:
    - bird: Objek burung yang dikendalikan pemain.
@@ -39,19 +30,16 @@ class FlappyBirdGame extends FlameGame with TapDetector{
     // Menambahkan burung ke dalam daftar komponen game.
     add(bird);
   }
-<<<<<<< HEAD
 }
-=======
+  
+/*
 
-  /*
+  TAP
 
-    TAP
+*/
 
-  */
-
-  @override
-  void onTap() {
-    bird.flap();
-  }
+@Override
+void onTap() {
+  bird.flap();
 }
->>>>>>> 2d5595c8838bfa7332e170da5e6f357631aac882
+}
