8943fb66d7d63988e614a3236e0ab4ae1e23b0c4
siddiqputra <siddiqanandaputra1@gmail.com>
2024-12-12 19:19:54 +0700

    Adding a Background

--- project/lib/constants.dart
+++ project/lib/constants.dart
@@ -6,3 +6,7 @@ const double birdWidth = 60;
 const double birdHeight = 40;
 const double gravity = 800;
 const double jumpStrength = -300;
+const double birdStartX = 100;
+const double birdStartY = 100;
+const double groundHeight = 150;
+const double groundScrollingSpeed = 100;
--- project/lib/game.dart
+++ project/lib/game.dart
@@ -3,11 +3,14 @@ import 'dart:async';
 // Mengimpor paket Flame untuk pengembangan game.
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'package:project/components/background.dart';
 // Mengimpor kelas Bird dari file komponen dalam proyek.
 import 'package:project/components/bird.dart';
+import 'package:project/components/ground.dart';
+import 'package:project/constants.dart';
 
-class FlappyBirdGame extends FlameGame with TapDetector {
-}
+class FlappyBirdGame extends FlameGame with TapDetector, HasCollisionDetection {
   /*
    Komponen dasar game:
    - bird: Objek burung yang dikendalikan pemain.
@@ -19,27 +22,78 @@ class FlappyBirdGame extends FlameGame with TapDetector {
 
   // Deklarasi variabel `bird` untuk menyimpan instance dari komponen Bird.
   late Bird bird;
+  late Background background;
+  late Ground ground;
 
   /*
     Metode onload dipanggil saat game dimuat.
     Digunakan untuk menginisialisasi komponen yang akan ditambahkan ke game.
   */
-  FutureOr<void> onload() {
-    // Memuat objek burung.
+  @override
+  FutureOr<void> onLoad() {
+    // Memuat latar belakang
+    background = Background(size);
+    add(background);
+
+    // Memuat burung
     bird = Bird();
-    // Menambahkan burung ke dalam daftar komponen game.
     add(bird);
+
+    // Memuat tanah
+    ground = Ground();
+    add(ground);
   }
-}
-  
-/*
 
-  TAP
+  /*
 
-*/
+    TAP
 
-@Override
-void onTap() {
-  bird.flap();
-}
+  */
+
+  @override
+  void onTap() {
+    bird.flap();
+  }
+
+  /*
+
+  GAME OVER
+
+  */
+
+  bool isGameOver = false;
+
+  void gameOver() {
+    // Mencegah pemicu game over yang ganda
+    if (isGameOver) return;
+
+    isGameOver = true;
+    pauseEngine();
+
+    // Tampilkan kotak dialog untuk pengguna
+    showDialog(
+      context: buildContext!,
+      builder: (context) => AlertDialog(
+        title: const Text("Game Over"),
+        actions: [
+          TextButton(
+              onPressed: () {
+                // Menutup kotak dialog
+                Navigator.pop(context);
+
+                // Mengatur ulang permainan
+                resetGame();
+              },
+              child: const Text("Restart"))
+        ],
+      ),
+    );
+  }
+
+  void resetGame() {
+    bird.position = Vector2(birdStartX, birdStartY);
+    bird.velocity = 0;
+    isGameOver = false;
+    resumeEngine();
+  }
 }
